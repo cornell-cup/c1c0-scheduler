14c72299f9438034eb4df75a2bf9fa7d7bd716ab
kyleinfantino <kyleinfantino@gmail.com>
2020-05-13 23:54:19 -0400

    example code for external interrupts

---
+++ interrupt.py
@@ -0,0 +1,22 @@
+#Test of Jetson External Interrupts
+#Add external interrupt on Jetson pin 18
+
+import Jetson.GPIO as GPIO
+import time
+
+def interrupted(channel):
+    print("interrupt")
+
+def main():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(18, GPIO.IN)
+    GPIO.add_event_detect(18, GPIO.FALLING, callback = interrupted)
+    print("starting")
+
+    while True:
+        for i in range(1000000):
+            if i == 10:
+                print("running")
+
+if __name__ == '__main__':
+    main()
