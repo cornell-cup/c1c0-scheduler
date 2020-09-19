8ec916983be9f1a6250718109b9f054d4bec9130
bsawka <bas335@cornell.edu>
2020-09-19 13:02:02 -0400

    new file for socket proof of concept

---
+++ socket_comm.py
@@ -0,0 +1,13 @@
+# socket_comm.py
+
+# Proof of concept for socket based per-process communication
+
+# Fall 2020
+
+import subprocess
+import multiprocessing
+import os
+import sys
+import socket
+
+
