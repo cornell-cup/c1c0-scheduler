c5868fb2f0f7fb4b531ea6bd6f81062e2b518f6a
AngelaZou2000 <az292@cornell.edu>
2020-12-21 20:30:32 +0800

    comments

--- scheduling.py
+++ scheduling.py
@@ -23,23 +23,24 @@ flag = 0 # for xbox controller
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            # if flog = 1:
-            # 	kill thread
-            # else if lock grabbed:
-            # 	conn.sendall("resource occupied")
-        	# else:
-            # 	spawn pathplanning or object detection
-	        # 		a thread with (program name, data)
-	        # 		thread will grab the lock, spawn the program, waiting for result to come back, release lock, return result
-            conn.sendall(data)
+def main_thread():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
+            while True:
+                data = conn.recv(1024)
+                # if flag = 1:
+                # 	kill thread
+                # else if lock grabbed:
+                # 	conn.sendall("resource occupied")
+            	# else:
+                # 	spawn pathplanning or object detection as a thread
+    	        # 		thread will grab the lock, spawn the program, 
+                #       waiting for result to come back, release lock, 
+                #       return result
 
 def function_thread(program, data):
 	lock.acquire()
@@ -54,4 +55,4 @@ def xbox_thread():
 	# waiting for interrupt
 	# kill function_thread (stop flag, release the lock if lock is grabbed)
 	# do whatever it is going to do
-	# flop the thread back to 0
+	# flip the thread back to 0
