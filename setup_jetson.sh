54d420fe5a905574b445e70053c5f9a573315807
Christopher De Jesus <christopher2653@gmail.com>
2023-03-11 16:44:59 -0500

    work on path-planning script

--- setup_jetson.sh
+++ setup_jetson.sh
@@ -21,26 +21,38 @@ info() { aqua && printf "$1" && none; }
 perr() { ruby && printf "$1" && none; }
 
 # Installation utilities
-try_install() { # $1 = package name, $2 = terminal command
+try_install() { # $1 = package name
     info "Installing $1...\n"
-    if ! [ -x "$(command -v $2)"]; then
-        if [ "$verbose" = true ]; then
-            sudo apt-get install "$1" || perr "Failed to install $1\n";
-        else
-            sudo apt-get install "$1" &> /dev/null || perr "Failed to install $1\n";
-        fi
+
+    dpkg -s "$1" &> /dev/null
+    if [ ! $? -eq 0 ]; then
+        if [ "$verbose" = true ]; then sudo apt-get install "$1" || perr "Failed to install $1\n";
+        else sudo apt-get install "$1" &> /dev/null || perr "Failed to install $1\n"; fi
     fi
+
+    dpkg -s "$1" &> /dev/null
+    if [ ! $? -eq 0 ]; then return 1;
+    else return 0; fi
 }
 
 try_clone() { # $1 = repo link, $2 = local path
     info "Cloning $1...\n"
+
     if [ ! -d "$2/.git" ]; then
-        if [ "$verbose" = true ]; then
-            git clone "$1" "$2" || perr "Failed to clone $1\n";
-        else
-            git clone "$1" "$2" &> /dev/null || perr "Failed to clone $1\n";
-        fi
+        if [ "$verbose" = true ]; then git clone "$1" "$2" || perr "Failed to clone $1\n";
+        else git clone "$1" "$2" &> /dev/null || perr "Failed to clone $1\n"; fi
     fi
+
+    if  [ ! -d "$2/.git" ]; then return 1;
+    else return 0; fi
+}
+
+try_checkout() { #1 = local path, $2 = branch name
+    if [ "$verbose" = true ]; then git -C $1 checkout $2 || perr "Failed to checkout $2\n";
+    else git -C $1 checkout $2 &> /dev/null || perr "Failed to checkout $2\n"; fi
+
+    if [ $(git rev-parse --abbrev-ref HEAD) != "$2" ]; then return 1;
+    else return 0; fi
 }
 
 # Install python related packages
@@ -49,5 +61,18 @@ try_install python3-dev
 try_install python3-pip
 try_install python3-venv
 
+# Set up path_planning
+path_continue=true
+path_remote="git@github.com:cornell-cup/C1C0_path_planning.git"
+path_local="../c1c0-path-planning"
+
+info "Building path planning...\n"
+if $path_continue; then try_clone $path_remote $path_local || path_continue=false; fi
+if $path_continue; then try_checkout $path_local "integration" || path_continue=false; fi
+if $path_continue; then python3 -m venv "${path_local}/venv" || path_continue=false; fi 
+if $path_continue; then source "${path_local}/venv/bin/activate" || path_continue=false; fi
+if $path_continue; then pip install -r "${path_local}/requirements.txt" || path_continue=false; fi
+if $path_continue; then deactivate || path_continue=false; fi
+  
 # Set up facial recognition
-try_clone git@github.com:cornell-cup/r2-facial_recognition_client.git ../r2-facial_recognition_client
+# try_clone git@github.com:cornell-cup/r2-facial_recognition_client.git ../r2-facial_recognition_client
