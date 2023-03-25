873ca42ea612f4e200609a495b314222f7651cef
Sayeem2004 <MohammadSSKC@gmail.com>
2023-03-25 15:45:11 -0400

    add virtual environment and pip installation to setup_jetson.sh

--- setup_jetson.sh
+++ setup_jetson.sh
@@ -1,8 +1,9 @@
 #-------------------------------------------------------------------------------
-# This script is used to setup an environment for the c1co project. It requires
+# This script is used to setup an environment for the C1C0 project. It requires
 # a linux environment with apt-get and git installed. It will install all the
-# repositories for the c1c0 modules. Credit for this script goes to  Christopher
-# De Jesus, and Mohammad Khan.
+# repositories for the C1C0 modules, set up their virtual environments, and
+# download requirements. Credit for this script goes to Christopher De Jesus,
+# and Mohammad Khan.
 #
 # Script Usage:
 # chmod +x setup_jetson.sh
@@ -13,15 +14,18 @@
 if [ "$1" = "-v" ]; then verbose=true;
 else verbose=false; fi
 
-# Pretty printing utilities
+# Changes terminal text color
 ruby() { printf "\e[1;31m"; }
 aqua() { printf "\e[1;32m"; }
 none() { printf "\e[0m"; }
+
+# Prints a message in color
 info() { aqua && printf "$1" && none; }
 perr() { ruby && printf "$1" && none; }
-bord() { aqua && echo "--------------------------------------------------------------------------" && none; }
+bord() { aqua && echo "----------------------------------------" && none; }
 
-# Installation utilities
+# Attempts to install the given linux package.
+# Returns 0 if successful, 1 if not.
 try_install() { # $1 = package name
     info "\tInstalling $1...\n"
 
@@ -36,7 +40,9 @@ try_install() { # $1 = package name
     else return 0; fi
 }
 
-try_clone() { # $1 = repo link, $2 = local path
+# Attempts to clone the given git repository to the given repo path.
+# Returns 0 if successful, 1 if not.
+try_clone() { # $1 = repo link, $2 = repo path
     info "\tCloning $1...\n"
 
     if [ ! -d "$2/.git" ]; then
@@ -48,15 +54,47 @@ try_clone() { # $1 = repo link, $2 = local path
     else return 0; fi
 }
 
-try_checkout() { #1 = local path, $2 = branch name
-    info "\tChecking $2...\n"
+# Attempts to checkout to the given branch of the given git repository.
+# Returns 0 if successful, 1 if not.
+try_checkout() { # $1 = repo path, $2 = branch name
+    info "\tChecking $1:$2...\n"
 
     if [ $(git -C $1 rev-parse --abbrev-ref HEAD) != $2 ]; then
-        if [ "$verbose" = true ]; then git -C $1 checkout $2 || perr "Failed to checkout $2\n";
-        else git -C $1 checkout $2 &> /dev/null || perr "Failed to checkout $2\n"; fi
+        if [ "$verbose" = true ]; then git -C $1 checkout $2 || perr "Failed to checkout $1:$2\n";
+        else git -C $1 checkout $2 &> /dev/null || perr "Failed to checkout $1:$2\n"; fi
     fi
 
     if [ $(git -C $1 rev-parse --abbrev-ref HEAD) != "$2" ]; then return 1;
+    else return 0; fi
+}
+
+# Attempts to create a python virtual environment in the given venv path.
+# Returns 0 if successful, 1 if not.
+try_venv() { # $1 = venv path
+    info "\tCreating $1...\n"
+
+    if [ ! -d "$1" ]; then
+        if [ "$verbose" = true ]; then python3 -m venv "$1" || perr "Failed to create $1\n";
+        else python3 -m venv "$1" &> /dev/null || perr "Failed to create $1\n"; fi
+    fi
+
+    if [ ! -d "$1" ]; then return 1;
+    else return 0; fi
+}
+
+# Attempts to install the given requirements file using the given pip path.
+# Returns 0 if successful, 1 if not.
+try_pip() { # $1 = pip path, $2 = requirements path
+    info "\tInstalling $1...\n"
+
+    $1 freeze -r $2 2>&1 > /dev/null | grep -q "not installed"
+    if [ ! $? -eq 0 ]; then
+        if [ "$verbose" = true ]; then $1 install -r $2 || perr "Failed to install $1\n";
+        else $1 install -r $2 &> /dev/null || perr "Failed to install $1\n"; fi
+    fi
+
+    $1 freeze -r $2 2>&1 > /dev/null | grep -q "not installed"
+    if [ ! $? -eq 0 ]; then return 1;
     else return 0; fi
 }
 
@@ -67,60 +105,85 @@ try_install python3-dev
 try_install python3-pip
 try_install python3-venv
 
-# Set up path planning
+# Path planning information
 path_continue=true
 path_remote="git@github.com:cornell-cup/C1C0_path_planning.git"
 path_local="../c1c0-path-planning"
+path_branch="integration"
+path_venv="$path_local/venv"
+path_pip="$path_venv/bin/pip"
+path_req="$path_local/requirements.txt"
 
+# Set up path planning
 bord && info "Building path planning...\n"
 if [ $path_continue = true ]; then try_clone $path_remote $path_local || path_continue=false; fi
-if [ $path_continue = true ]; then try_checkout $path_local "integration" || path_continue=false; fi
+if [ $path_continue = true ]; then try_checkout $path_local $path_branch || path_continue=false; fi
+if [ $path_continue = true ]; then try_venv $path_venv || path_continue=false; fi
+if [ $path_continue = true ]; then try_pip $path_pip $path_req || path_continue=false; fi
 if [ $path_continue = false ]; then perr "Failed to build path planning\n"; fi
 
-# if $path_continue; then python3 -m venv "${path_local}/venv" || path_continue=false; fi 
-# if $path_continue; then source "${path_local}/venv/bin/activate" || path_continue=false; fi
-# if $path_continue; then pip install -r "${path_local}/requirements.txt" || path_continue=false; fi
-# if $path_continue; then deactivate || path_continue=false; fi
-
-# Set up facial recognition
+# Facial recognition information
 facial_continue=true
 facial_remote="git@github.com:cornell-cup/r2-facial_recognition_client.git"
 facial_local="../r2-facial_recognition_client"
+facial_branch="updating"
+facial_venv="$facial_local/venv"
+facial_pip="$facial_venv/bin/pip"
+facial_req="$facial_local/requirements.txt"
 
+# Set up facial recognition
 bord && info "Building facial recognition...\n"
 if [ $facial_continue = true ]; then try_clone $facial_remote $facial_local || facial_continue=false; fi
-if [ $facial_continue = true ]; then try_checkout $facial_local "updating" || facial_continue=false; fi
-if [ $facial_continue = false ]; then perr "Failed to build facial recognition\n"; fi 
+if [ $facial_continue = true ]; then try_checkout $facial_local $facial_branch || facial_continue=false; fi
+if [ $facial_continue = true ]; then try_venv $facial_venv || facial_continue=false; fi
+if [ $facial_continue = true ]; then try_pip $facial_pip $facial_req || facial_continue=false; fi
+if [ $facial_continue = false ]; then perr "Failed to build facial recognition\n"; fi
 
-# Set up chatbot
+# Chatbot information
 chat_continue=true
 chat_remote="git@github.com:cornell-cup/r2-chatbot.git"
 chat_local="../r2-chatbot"
+chat_branch="master"
+chat_venv="$chat_local/r2_chatterbot/venv"
+chat_pip="$chat_venv/bin/pip"
+chat_req="$chat_local/r2_chatterbot/requirements.txt"
 
+# Set up chatbot
 bord && info "Building chatbot...\n"
 if [ $chat_continue = true ]; then try_clone $chat_remote $chat_local || chat_continue=false; fi
-if [ $chat_continue = true ]; then try_checkout $chat_local "master" || chat_continue=false; fi
+if [ $chat_continue = true ]; then try_checkout $chat_local $chat_branch || chat_continue=false; fi
+if [ $chat_continue = true ]; then try_venv $chat_venv || chat_continue=false; fi
+if [ $chat_continue = true ]; then try_pip $chat_pip $chat_req || chat_continue=false; fi
 if [ $chat_continue = false ]; then perr "Failed to build chatbot\n"; fi
 
-# Set up object detection
+# Object detection information
 object_continue=true
 object_remote="git@github.com:cornell-cup/r2-object_detection.git"
 object_local="../r2-object_detection"
+object_branch="blue-arm"
+object_venv="$object_local/venv"
+object_pip="$object_venv/bin/pip"
+object_req="$object_local/requirements.txt"
 
+# Set up object detection
 bord && info "Building object detection...\n"
 if [ $object_continue = true ]; then try_clone $object_remote $object_local || object_continue=false; fi
-if [ $object_continue = true ]; then try_checkout $object_local "blue-arm" || object_continue=false; fi
+if [ $object_continue = true ]; then try_checkout $object_local $object_branch || object_continue=false; fi
+if [ $object_continue = true ]; then try_venv $object_venv || object_continue=false; fi
+if [ $object_continue = true ]; then try_pip $object_pip $object_req || object_continue=false; fi
 if [ $object_continue = false ]; then perr "Failed to build objection detection\n"; fi
 
-# Set up movement
+# Movement information
 move_continue=true
 move_remote="git@github.com:cornell-cup/c1c0-movement.git"
 move_local="../c1c0-movement"
+move_branch="main"
 
+# Set up movement
 bord && info "Building movement...\n"
 if [ $move_continue = true ]; then try_clone $move_remote $move_local || move_continue=false; fi
-if [ $move_continue = true ]; then try_checkout $move_local "main" || move_continue=false; fi
+if [ $move_continue = true ]; then try_checkout $move_local $move_branch || move_continue=false; fi
 if [ $move_continue = false ]; then perr "Failed to build movement\n"; fi
 
 # Ending script
-bord
+bord && exit 0
