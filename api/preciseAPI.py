83258cffbd3c2af9fdf6353e4f32ec4885b32aa5
Sayeem2004 <MohammadSSKC@gmail.com>
2024-04-13 15:17:43 -0400

    formatting

--- api/preciseAPI.py
+++ api/preciseAPI.py
@@ -4,6 +4,7 @@ from typing import List # Typing
 
 motor_angles = [0,0,0,0,0,0,0]
 motor_index  = 0
+state        = 0
 
 def update_precise(axis_x, axis_y) -> None:
     """
@@ -48,27 +49,20 @@ def get_precise() -> str:
     global motor_angles
     return precise_encode(motor_angles)
 
-state = 0
 def example_precise() -> str:
     """
     Creates an example message with motor angles to send to scheduler.
 
     @return: The example message with motor angles.
     """
-    global state
-    state+=1
-    if(state == 4):
-        state = 0
+    global state, motor_angles
+    state = (state + 1) % 4
 
-    global motor_angles
-    if(state == 0):
-        motor_angles = [30, 75, 40, 80, 30, 5, motor_angles[6]]
-    elif(state == 1):
-        motor_angles = [60, 65, 10, 60, 90, 30, motor_angles[6]]
-    elif(state == 2):
-        motor_angles = [20, 55, 80, 10, 10, 50, motor_angles[6]]
-    else:
-        motor_angles = [30, 75, 0, 60, 30, 30, motor_angles[6]]
+    if (state == 0):   motor_angles = [30, 75, 40, 80, 30,  5, motor_angles[6]]
+    elif (state == 1): motor_angles = [60, 65, 10, 60, 90, 30, motor_angles[6]]
+    elif (state == 2): motor_angles = [20, 55, 80, 10, 10, 50, motor_angles[6]]
+    else:              motor_angles = [30, 75, 0, 60, 30, 30, motor_angles[6]]
+
     return precise_encode(motor_angles)
 
 def zero_precise() -> str:
