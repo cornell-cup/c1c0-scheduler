d0885531743db494716395181a44b06e688b0e78
Sayeem2004 <MohammadSSKC@gmail.com>
2024-12-07 13:11:26 -0500

    updating chatbot

--- clients/chatbot.py
+++ clients/chatbot.py
@@ -10,12 +10,13 @@ from client.client import OpenAPI # Client Interface
 from client.config import FILE_MODE # Configuration
 
 from labels.config import recognize as config_recognize, handler as config_handler  # Configuration Specifications
-from labels.general import recognize as general_recognize, handler as general_handler  # General Info Specifications
-from labels.movement import recognize as movement_recognize, handler as movement_handler  # Movement Specifications
 from labels.facial import recognize as facial_recognize, handler as facial_handler  # Facial Specifications
+from labels.general import recognize as general_recognize, handler as general_handler # General Specifications
+from labels.movement import recognize as movement_recognize, handler as movement_handler  # Movement Specifications
+from labels.question import recognize as question_recognize, handler as question_handler  # Question Specifications
 
-import numpy as np
 from typing import Callable, Dict # Type Hinting
+
 
 if __name__ == '__main__':
     # Initializing clients
@@ -23,12 +24,22 @@ if __name__ == '__main__':
     scheduler_client: SClient = SClient('chatbot')
     scheduler_client.connect()
 
-    # Initializing response handlers and mapping
+    # Initialzing response handlers and mapping
     mapping: Dict[str, Callable[[str], None]] = {
-        facial_recognize:   lambda msg: facial_handler(chatbot_client, msg, scheduler_client),
-        movement_recognize: lambda msg: movement_handler(chatbot_client, msg, scheduler_client),
-        general_recognize:  lambda msg: general_handler(chatbot_client, msg, scheduler_client),
         config_recognize:   lambda msg: config_handler(chatbot_client, msg, scheduler_client),
+        facial_recognize:   lambda msg: facial_handler(chatbot_client, msg, scheduler_client),
+        general_recognize:  lambda msg: general_handler(chatbot_client, msg, scheduler_client),
+        movement_recognize: lambda msg: movement_handler(chatbot_client, msg, scheduler_client),
+        question_recognize: lambda msg: question_handler(chatbot_client, msg, scheduler_client),
+    }
+
+    # Initialize threshold for each task
+    thresholds: Dict[str, int] = {
+        config_recognize: 0.8,
+        facial_recognize: 0.5,
+        movement_recognize: 0.4,
+        question_recognize: 0.3,
+        general_recognize: 0.3,
     }
 
     # Infinite loop for chatbot
@@ -37,11 +48,12 @@ if __name__ == '__main__':
         msg: str = file_to_text() if FILE_MODE else speech_to_text()
         print(f"\033[32mUser: {msg}\033[0m")
 
-        # Checking and removing C1C0 name from message
-        if (not msg) or (not remove_C1C0(msg)) or (not recognize_C1C0(msg)):
+        # Checking and converting STT message
+        if msg is None or not recognize_C1C0(msg):
             print('C1C0 Command Not Recognized.')
             continue
         msg = remove_C1C0(msg)
+        msg = convert_C1C0(msg)
 
         # Finding and calling handler for message
         print(f"\033[32mCommand: {msg}\033[0m")
@@ -49,14 +61,16 @@ if __name__ == '__main__':
 
         for recognize, handler in mapping.items():
             score = recognize(chatbot_client, msg)
-            if score>best_score:
+            if score-thresholds[recognize] > best_score:
                 best_handler, best_score = handler, score
 
-        # Response sequence
+        play_random_sound()
+        if best_handler: text_to_speech(best_handler(msg))
+        else: text_to_speech("I did not understand the message. Please repeat it again or elaborate.")
         play_random_sound()
         text = best_handler(msg)
 
-        if text is not None:
-            print(text)
-            text_to_speech(text)
-            play_random_sound()
+        # Storing previous messages
+        chatbot_client.previous.append(msg)
+        if len(chatbot_client.previous) > 5:
+            chatbot_client.previous.remove(0)
