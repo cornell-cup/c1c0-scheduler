f56e50e92a844c82c9029392471e7088bb875437
Sayeem2004 <MohammadSSKC@gmail.com>
2025-03-08 12:26:43 -0500

    clean exit of scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,10 @@
 import numpy as np # Standard Python Imports
+import signal, time
+
+from api.locomotionAPI import zero_locomotion # Locomotion Utilities
+from api.preciseAPI import zero_precise # Precise Utilities
+from api.rotateAPI import zero_rotate # Rotate Utilities
+from api.strongAPI import zero_strong # Strong Utilities
 
 from scheduler.camera import Camera # Camera
 from scheduler.config import * # Configuration
@@ -13,11 +19,29 @@ from specs.controller import controller_check, controller_put # Controller Speci
 
 from typing import Callable, Dict, Union # Type Hinting
 
+
+def clean_exit(sig, frame) -> None:
+    """
+    Function to perform a clean exit, when the program is interrupted.
+    """
+    global scheduler, queue, camera
+    queue.add(Message('xbox', 'put', zero_locomotion()))
+    queue.add(Message('xbox', 'put', zero_strong()))
+    queue.add(Message('xbox', 'put', zero_precise()))
+    queue.add(Message('xbox', 'put', zero_rotate()))
+    print("Performing Clean Exit Of Scheduler...")
+
+
 if __name__ == '__main__':
     # Initializing server and data queue
     scheduler: Server = Server()
     queue: DataQueue = DataQueue()
     camera: Camera = Camera()
+
+    # Setting up signal handler
+    signal.signal(signal.SIGTERM, clean_exit)
+    kill_now: bool = False
+    kill_num: int = 20
 
     # Opening Camera
     with camera as cam:
@@ -52,9 +76,10 @@ if __name__ == '__main__':
             attempt += 1
 
         # Infinite loop for server
-        while True:
+        while kill_num > 0:
             # Receiving message from client
             msg: Message = scheduler.receive()
+            if (kill_now): kill_num -= 1
 
             # Finding and calling handler for message
             search: str = f'{msg.name}{TAG_SEP}{msg.tag}'
